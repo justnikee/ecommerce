f48f27ebc6b4b269ed0a7ac6d39551c833e8c844
justnikee <hk5561111212@gmail.com>
2022-09-30 23:27:40 +0530

    firebase authintacation and signin and signout done with firebase

--- src/App.js
+++ src/App.js
@@ -6,21 +6,44 @@ import Homepage from './page/homepage/homepage_component';
 import ShopPage from './page/homepage/shoppage/shoppage.component';
 import Header from './components/header-component';
 import SigninAndSignUpPage from './page/homepage/Signin and signup page/Signin-and-signup-component';
+import { auth } from './firebase/firebase.utils'
 
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
 
-function App() {
-  return (
-    <Router>
-      <div>
-      <Header />
-        <Routes>
-          <Route exact path='/' element={ <Homepage/> } />
-          <Route path='/shop' element={ <ShopPage/> } />
-          <Route path='/signin' element={ <SigninAndSignUpPage /> } />
-        </Routes>
-      </div> 
-    </Router> 
-  );
+  unsubscribeDromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeDromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser : user })
+    console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeDromAuth();
+  }
+  
+  render(){
+    return (
+      <Router>
+        <div>
+        <Header currentUser={this.state.currentUser} />
+          <Routes>
+            <Route exact path='/' element={ <Homepage/> } />
+            <Route path='/shop' element={ <ShopPage/> } />
+            <Route path='/signin' element={ <SigninAndSignUpPage /> } />
+          </Routes>
+        </div> 
+      </Router> 
+    );
+  }
+  
 }
 
 export default App;
